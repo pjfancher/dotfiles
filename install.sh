18053638a6885574700c194cceb073d3105db3a3
PJ Fancher <pj@digitalbrands.com>
2016-10-19 19:14:22 +0000

    Added install scripts

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/db-pj/bashrc ~/bashrc && cd ~/bashrc && ./install && cd ~ && . .bashrc
+
+git clone https://github.com/db-pj/vimrc ~/vimrc && cd ~/vimrc && ./install && cd ~
