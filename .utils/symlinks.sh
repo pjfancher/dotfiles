c5bc432dc42e90e32ae6c69f11387f7e514ea916
PJ Fancher <pj@digitalbrands.com>
2016-10-21 16:01:58 +0000

    update install

---
+++ .utils/symlinks.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# this symlinks all the dotfiles (and .vim/) to ~/
+
+# finds all .dotfiles in this folder
+declare -a FILES_TO_SYMLINK=$(find . -type f -maxdepth 1 -name ".*" -not -name .DS_Store -not -name .git -not -name .osx | sed -e 's|//|/|' | sed -e 's|./.|.|')
+FILES_TO_SYMLINK="$FILES_TO_SYMLINK .vim" # add in vim and the binaries
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    local i=""
+    local sourceFile=""
+    local targetFile=""
+
+    for i in ${FILES_TO_SYMLINK[@]}; do
+
+        sourceFile="$(pwd)/$i"
+        targetFile="$HOME/$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+
+        if [ -e "$targetFile" ]; then
+            if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
+
+                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+                if answer_is_yes; then
+                    rm -rf "$targetFile"
+                    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+                else
+                    print_error "$targetFile → $sourceFile"
+                fi
+
+            else
+                print_success "$targetFile → $sourceFile"
+            fi
+        else
+            execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+        fi
+    done
+}
+
+main
