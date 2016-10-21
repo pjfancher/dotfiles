a1edb756a9a00d2046dbe0655f5c147cd08ecbf9
PJ Fancher <pj@digitalbrands.com>
2016-10-21 20:40:36 +0000

    Force file update

--- .utils/symlinks.sh
+++ .utils/symlinks.sh
@@ -24,13 +24,8 @@ main() {
         if [ -e "$targetFile" ]; then
             if [ "$(readlink "$targetFile")" != "$sourceFile" ]; then
 
-                ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
-                if answer_is_yes; then
-                    rm -rf "$targetFile"
-                    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
-                else
-                    print_error "$targetFile → $sourceFile"
-                fi
+                rm -rf "$targetFile"
+                execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
 
             else
                 print_success "$targetFile → $sourceFile"
