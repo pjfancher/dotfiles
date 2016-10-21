17c37af965af99551d97cba3a6e60253775c6adb
PJ Fancher <pj@digitalbrands.com>
2016-10-21 14:11:16 -0400

    Update install-remote.sh

--- install-remote.sh
+++ install-remote.sh
@@ -3,4 +3,4 @@
 # peej.io forwards to this file
 # curl -L peej.io | bash
 
-git clone https://github.com/db-pj/dotfiles $HOME && . $HOME/dotfiles/install.sh
+git clone https://github.com/db-pj/dotfiles $HOME/dotfiles && . $HOME/dotfiles/install.sh
