e4e23c5cc96ba0b316388a76414d051d64562ddc
PJ Fancher <pj@digitalbrands.com>
2016-11-01 14:50:29 +0000

    install fix

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 # Install oh-my-zsh
 # https://github.com/robbyrussell/oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl -OL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+bash install.sh
 
 # Symlinks & Vim Plugins
        . "$HOME/dotfiles/.utils/utils.sh" \
