1464c3675931bfd8fb63b1bc55a91d680f13a1c3
PJ Fancher <pj@digitalbrands.com>
2016-11-01 14:22:32 +0000

    Added custom dir

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cd "$(dirname "${BASH_SOURCE[0]}")" \
         && . ".utils/utils.sh" \
         && . ".utils/symlinks.sh" \
         && . ".bashrc" \
         && . ".utils/vim.sh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 printf "\n\n"
