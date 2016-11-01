7b7e85ef100016e2eab476d164da3b445908f4d9
PJ Fancher <pj@digitalbrands.com>
2016-11-01 11:15:02 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-   . "$HOME/dotfiles/.utils/utils.sh" \
-&& . "$HOME/dotfiles/.utils/symlinks.sh" \
-&& . "$HOME/dotfiles/.utils/vim.sh"
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+   && . ".utils/utils.sh" \
+   && . ".utils/symlinks.sh" \
+   && . ".utils/vim.sh"
 printf "\n\n"
 
 # Install oh-my-zsh
 # https://github.com/robbyrussell/oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 printf "\n\n"
-
-
-#cd "$(dirname "${BASH_SOURCE[0]}")" \
-#        && . ".utils/utils.sh" \
-#        && . ".utils/symlinks.sh" \
-#        && . ".utils/vim.sh"
-#printf "\n\n"
