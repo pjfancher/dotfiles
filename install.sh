b07065c1f9ebede1f74659f4dd44d7d412a638ac
PJ Fancher <pj@digitalbrands.com>
2016-11-01 11:53:44 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,8 +10,7 @@ printf "\n\n"
 
 # Install oh-my-zsh
 # https://github.com/robbyrussell/oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" \
-printf "\n\n"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Use Custom .zshrc
-zsh -c "mv $HOME/.zshrc.pre-oh-my-zsh $HOME/.zshrc && . $HOME/.zshrc"
+mv $HOME/.zshrc.pre-oh-my-zsh $HOME/.zshrc && source $HOME/.zshrc
