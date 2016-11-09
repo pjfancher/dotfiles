bd35169b36de28fc34fe7d1d359f81e339f96cf6
PJ Fancher <pj@digitalbrands.com>
2016-11-09 12:55:41 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,7 @@
 cd "$(dirname "${BASH_SOURCE[0]}")" \
    && . ".utils/utils.sh" \
    && . ".utils/symlinks.sh" \
+   && . ".utils/vim.sh" \
    && . ".utils/zsh.sh" \
    && cd $HOME
 printf "\n\n"
-
-# Install oh-my-zsh
-# https://github.com/robbyrussell/oh-my-zsh
-# bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Use Custon .zshrc
-#mv $HOME/.zshrc.pre-oh-my-zsh $HOME/.zshrc \
-#&& source $HOME/.zshrc
