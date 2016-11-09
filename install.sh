5b41eb8d2fa1a1e161dae1f20e34bc96b1e0716d
PJ Fancher <pj@digitalbrands.com>
2016-11-09 12:54:10 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@
 cd "$(dirname "${BASH_SOURCE[0]}")" \
    && . ".utils/utils.sh" \
    && . ".utils/symlinks.sh" \
-#   && . ".utils/vim.sh" \
    && . ".utils/zsh.sh" \
    && cd $HOME
 printf "\n\n"
