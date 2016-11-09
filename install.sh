0241523ab187a7a110806930051a8d4b0f7e890b
PJ Fancher <pj@digitalbrands.com>
2016-11-09 12:28:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,3 +15,16 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/m
 # Use Custon .zshrc
 mv $HOME/.zshrc.pre-oh-my-zsh $HOME/.zshrc \
 && source $HOME/.zshrc
+
+TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+  if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+    # If this platform provides a "chsh" command (not Cygwin), do it, man!
+    if hash chsh >/dev/null 2>&1; then
+      printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
+      chsh -s $(grep /zsh$ /etc/shells | tail -1)
+    # Else, suggest the user do so manually.
+    else
+      printf "I can't change your shell automatically because this system does not have chsh.\n"
+      printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
+    fi
+  fi
