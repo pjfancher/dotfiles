5615104f40aa5bd60043ac6480e4d59a4af144cc
PJ Fancher <pj@digitalbrands.com>
2016-11-09 16:44:33 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
    && . ".utils/utils.sh" \
    && . ".utils/symlinks.sh" \
    && . ".utils/zsh.sh" \
+   && . ".utils/vim.sh" \
    && cd $HOME
 
 print_in_purple "\n\n   Environment Setup Complete! Enjoy!\n"
