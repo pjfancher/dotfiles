945a056f9bc6170aa79dc27e66de7974ecba55a3
PJ Fancher <pj@digitalbrands.com>
2016-11-09 12:36:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,26 +5,14 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
    && . ".utils/utils.sh" \
    && . ".utils/symlinks.sh" \
    && . ".utils/vim.sh" \
+   && . ".utils/zsh.sh" \
    && cd $HOME
 printf "\n\n"
 
 # Install oh-my-zsh
 # https://github.com/robbyrussell/oh-my-zsh
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Use Custon .zshrc
 mv $HOME/.zshrc.pre-oh-my-zsh $HOME/.zshrc \
 && source $HOME/.zshrc
-
-TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
-  if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
-    # If this platform provides a "chsh" command (not Cygwin), do it, man!
-    if hash chsh >/dev/null 2>&1; then
-      printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
-      chsh -s $(grep /zsh$ /etc/shells | tail -1)
-    # Else, suggest the user do so manually.
-    else
-      printf "I can't change your shell automatically because this system does not have chsh.\n"
-      printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
-    fi
-  fi
