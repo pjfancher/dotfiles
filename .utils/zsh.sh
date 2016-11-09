89b57c4ca5bee434b8ea75887219f5c1a5a13f8a
PJ Fancher <pj@digitalbrands.com>
2016-11-09 16:25:59 -0500

    Update zsh.sh

--- .utils/zsh.sh
+++ .utils/zsh.sh
@@ -87,8 +87,8 @@ main() {
   if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
     # If this platform provides a "chsh" command (not Cygwin), do it, man!
     if hash chsh >/dev/null 2>&1; then
-      printf "${GREEN}\n   Time to change your default shell to zsh${NORMAL}\n"
-      echo '   ' && chsh -s $(grep /zsh$ /etc/shells | tail -1) < /dev/tty
+      printf "${GREEN}   Time to change your default shell to zsh${NORMAL}"
+      printf "\t" && chsh -s $(grep /zsh$ /etc/shells | tail -1) < /dev/tty
     # Else, suggest the user do so manually.
     else
       printf "I can't change your shell automatically because this system does not have chsh.\n"
