4c91699837d921e9a375e54a0d4135e47e059ff5
PJ Fancher <pj@digitalbrands.com>
2016-11-10 10:43:16 -0500

    Update zsh.sh

--- .install/zsh.sh
+++ .install/zsh.sh
@@ -87,11 +87,10 @@ main() {
   if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
     # If this platform provides a "chsh" command (not Cygwin), do it, man!
     if hash chsh >/dev/null 2>&1; then
-      printf "${GREEN}   Changing your default shell to zsh...${NORMAL}\n\t"
+      printf "${GREEN}   \nChanging your default shell to zsh...${NORMAL}\n\t"
       chsh -s $(grep /zsh$ /etc/shells | tail -1) < /dev/tty
       print_success '   Shell changed to zsh!'
       print_warning '   Log out and log back in to complete shell switch'
-      print_warning '   Or run: env zsh'
     # Else, suggest the user do so manually.
     else
       printf "I can't change your shell automatically because this system does not have chsh.\n"
