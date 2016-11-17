5c6047a26a09c4768664dd0c610aec6b3ced1a34
PJ Fancher <pj@digitalbrands.com>
2016-11-17 20:02:17 +0000

    update

--- .zsh/lib/correction.zsh
+++ .zsh/lib/correction.zsh
@@ -1,14 +1,14 @@
-if [[ "$ENABLE_CORRECTION" == "true" ]]; then
-  alias ebuild='nocorrect ebuild'
-  alias gist='nocorrect gist'
-  alias heroku='nocorrect heroku'
-  alias hpodder='nocorrect hpodder'
-  alias man='nocorrect man'
-  alias mkdir='nocorrect mkdir'
-  alias mv='nocorrect mv'
-  alias mysql='nocorrect mysql'
-  alias sudo='nocorrect sudo'
-  alias vim='nocorrect vim'
+#if [[ "$ENABLE_CORRECTION" == "true" ]]; then
+  #alias ebuild='nocorrect ebuild'
+  #alias gist='nocorrect gist'
+  #alias heroku='nocorrect heroku'
+  #alias hpodder='nocorrect hpodder'
+  #alias man='nocorrect man'
+  #alias mkdir='nocorrect mkdir'
+  #alias mv='nocorrect mv'
+  #alias mysql='nocorrect mysql'
+  #alias sudo='nocorrect sudo'
+  #alias vim='nocorrect vim'
 
-  setopt correct_all
-fi
+  #setopt correct_all
+#fi
