8e2e7e02cd21d9869932042154faa59c70c84361
PJ Fancher <pj@digitalbrands.com>
2016-11-21 16:27:16 +0000

    Added apt-get install

---
+++ .install/install-apt.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+. utils.sh
+
+# Ubuntu-only stuff. Abort if not Ubuntu.
+is_ubuntu || return 1
+
+ask_for_sudo
+
+print_in_purple "\n   Apt-Get\n   ------------------------------\n"
+packages=(
+    python-software-properties
+    software-properties-common
+    php5 
+    php5-gd 
+    php5-mysql 
+    php5-curl
+    htop
+    curl
+    mosh
+    dtrx
+    ncdu
+    tig
+    phpmyadmin
+)
+packages=($(setdiff "${packages[*]}" "$(dpkg --get-selections | grep -v deinstall | awk '{print $1}')"))
+#printf '%s\n' "${packages[@]}"
+
+
+if (( ${#packages[@]} > 0 )); then
+  for package in "${packages[@]}"; do
+    execute "sudo apt-get -qq install $package -y >/dev/null 2>&1" "Installing $package"
+  done
+fi
+
+print_success "All Packages Installed"
--- .install/utils.sh
+++ .install/utils.sh
@@ -349,3 +349,46 @@ show_spinner() {
     done
 
 }
+
+# Given strings containing space-delimited words A and B, "setdiff A B" will
+# return all words in A that do not exist in B. Arrays in bash are insane
+# (and not in a good way).
+# From http://stackoverflow.com/a/1617303/142339
+function setdiff() {
+  local debug skip a b
+  if [[ "$1" == 1 ]]; then debug=1; shift; fi
+  if [[ "$1" ]]; then
+    local setdiffA setdiffB setdiffC
+    setdiffA=($1); setdiffB=($2)
+  fi
+  setdiffC=()
+  for a in "${setdiffA[@]}"; do
+    skip=
+    for b in "${setdiffB[@]}"; do
+      [[ "$a" == "$b" ]] && skip=1 && break
+    done
+    [[ "$skip" ]] || setdiffC=("${setdiffC[@]}" "$a")
+  done
+  [[ "$debug" ]] && for a in setdiffA setdiffB setdiffC; do
+    echo "$a ($(eval echo "\${#$a[*]}")) $(eval echo "\${$a[*]}")" 1>&2
+  done
+  [[ "$1" ]] && echo "${setdiffC[@]}"
+}
+
+# If this file was being sourced, exit now.
+[[ "$1" == "source" ]] && return
+
+# OS detection
+function is_osx() {
+  [[ "$OSTYPE" =~ ^darwin ]] || return 1
+}
+
+function is_ubuntu() {
+  [[ "$(cat /etc/issue 2> /dev/null)" =~ Ubuntu ]] || return 1
+}
+
+function get_os() {
+  for os in osx ubuntu; do
+    is_$os; [[ $? == ${1:-0} ]] && echo $os
+  done
+}
