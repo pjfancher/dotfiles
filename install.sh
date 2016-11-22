df5287c2c605fa13e8e4b857b26c02f3adb3952b
PJ Fancher <pj@digitalbrands.com>
2016-11-22 15:14:06 +0000

    renamed apt/npm

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
   && . ".install/utils.sh" \
   && . ".install/banner.sh" \
   && . ".install/symlinks.sh" \
-  && . ".install/install-apt.sh" \
+  && . ".install/apt.sh" \
   && . ".install/zsh.sh" \
   && . ".install/vim.sh" \
   && cd $HOME
