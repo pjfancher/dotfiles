47318816ef8f4112df7009255b05ea480d3953d7
PJ Fancher <pj@digitalbrands.com>
2016-11-23 17:04:51 +0000

    Added git check for remote install script

--- install-remote.sh
+++ install-remote.sh
@@ -3,10 +3,18 @@
 # peej.io forwards to this file
 # curl -L peej.io | bash
 
+# Check for git
+hash git >/dev/null 2>&1 || {
+  echo "Error: git is not installed. Trying to install..."
+  sudo apt-get install git
+}
+
+# Delete current dotfiles dir if there is one
 if [ -d ~/dotfiles ]; then
   rm -rf $HOME/dotfiles
 fi
 
+# Clone the repo and run the install script
 cd $HOME \
 && git clone --quiet https://github.com/db-pj/dotfiles &> /dev/null \
 && . $HOME/dotfiles/install.sh
