32148a47c4df675f5a977fe1eb4c9e7dac86aca7
PJ Fancher <pj@digitalbrands.com>
2016-12-02 16:12:14 -0500

    Update docker.sh

--- install/packages/docker.sh
+++ install/packages/docker.sh
@@ -3,6 +3,9 @@
 #      Install Docker           #
 #################################
 
+# Ubuntu-only stuff. Abort if not Ubuntu.
+is_ubuntu || return 1
+
 ask_for_sudo
 print_in_purple "\n   Docker\n   ------------------------------\n"
 
