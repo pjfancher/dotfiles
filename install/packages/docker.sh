9cfff223a2221cb1cee1a740664a9befa0a701c5
PJ Fancher <pj@digitalbrands.com>
2016-12-02 16:11:39 -0500

    Update docker.sh

--- install/packages/docker.sh
+++ install/packages/docker.sh
@@ -9,8 +9,8 @@ print_in_purple "\n   Docker\n   ------------------------------\n"
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D \
 && sudo bash -c "echo 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' > /etc/apt/sources.list.d/docker.list" \
 && sudo apt-get update && sudo apt-get purge lxc-docker && apt-cache policy docker-engine \
-&& sudo apt-get update && sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual \
-&& sudo apt-get update && sudo apt-get install docker-engine && sudo service docker start \
+&& sudo apt-get update && sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y \
+&& sudo apt-get update && sudo apt-get install docker-engine -y && sudo service docker start \
 && sudo bash -c "curl -L https://github.com/docker/compose/releases/download/1.8.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose" \
 && sudo chmod +x /usr/local/bin/docker-compose \
 && sudo adduser pj docker
