8e02924127c13da99e0acfc0a4db437828da427b
PJ Fancher <pj@digitalbrands.com>
2017-01-17 10:23:35 -0500

    Update symlinks.sh

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -4,7 +4,7 @@
 
 # finds all .dotfiles in this folder
 declare -a FILES_TO_SYMLINK=$(find . -maxdepth 1 -type f -name ".*" -not -name .DS_Store -not -name .git -not -name .osx | sed -e 's|//|/|' | sed -e 's|./.|.|')
-FILES_TO_SYMLINK="$FILES_TO_SYMLINK .vim .zsh" # add in vim and the binaries
+FILES_TO_SYMLINK="$FILES_TO_SYMLINK .vim .zsh .tmuxp" # add in directories
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
