04bbd0ca8f0de4a966f7bcd45593d356b56e6194
PJ Fancher <pj@digitalbrands.com>
2017-06-26 10:02:12 -0400

    Update install-remote.sh

--- install-remote.sh
+++ install-remote.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
+#############################################################
 # peej.io forwards to this file
 # curl -L peej.io | bash
+#############################################################
 
 # Check for git
+#############################################################
 hash git >/dev/null 2>&1 || {
   echo "Error: git is not installed. Trying to install..."
   sudo apt-get install git
 }
 
 # Check for zsh
+#############################################################
 hash zsh >/dev/null 2>&1 || {
   echo "Error: zsh is not installed. Trying to install..."
   sudo apt-get install zsh
 }
 
 # Delete current dotfiles dir if there is one
+#############################################################
 if [ -d ~/dotfiles ]; then
   rm -rf $HOME/dotfiles
 fi
 
 # Clone the repo and run the install script
+#############################################################
 cd $HOME \
 && git clone --quiet --recursive https://github.com/db-pj/dotfiles &> /dev/null \
 && . $HOME/dotfiles/install.sh
