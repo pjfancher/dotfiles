3bc7c380c139358bef06cf12851014f085a9721a
PJ Fancher <pj@digitalbrands.com>
2018-05-08 16:07:29 +0000

    master@pj.passprotect.me: Split out LAMP packages and desired utilites

--- install-packages.sh
+++ install-packages.sh
@@ -7,9 +7,10 @@ ask_for_sudo
 
 # Load Scripts
 cd "$(dirname "${BASH_SOURCE[0]}")" \
-  && . "install/packages/apt.sh" \
+  && . "install/packages/lamp" \
   && . "install/packages/pip.sh" \
   && . "install/packages/npm.sh" \
+  && . "install/packages/utilities.sh" \
   && cd $HOME
 
 #Complete
---
+++ install/packages/utilities.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+###########################################
+#      Install apt-get packages.          #
+###########################################
+
+# Ubuntu-only stuff. Abort if not Ubuntu.
+is_ubuntu || return 1
+
+ask_for_sudo
+print_in_purple "\n   Apt-Get\n   ------------------------------\n"
+
+# Define needed packages
+packages=(
+    build-essential
+    npm
+    sendmail
+    libjpeg-progs
+    gifsicle
+    optipng
+    imagemagick
+    finger
+    vim-nox
+    ctags
+    tmux
+    htop
+    curl
+    mosh
+    dtrx
+    ncdu
+    tig
+    ack-grep
+    silversearcher-ag
+)
+
+# Remove packages already installed from list
+packages=($(setdiff "${packages[*]}" "$(dpkg --get-selections | grep -v deinstall | awk '{print $1}')"))
+
+# If we still have packages that need to be installed
+if (( ${#packages[@]} > 0 )); then
+
+  # Update Packages
+  print_success "Updating Packages..."
+  sudo apt-get -qq update > /dev/null 2>&1
+
+  # Install each package
+  for package in "${packages[@]}"; do
+    print_success "Installing $package..."
+    eval "sudo apt-get -qq install $package -y"
+  done
+
+  # Upgrade Packages
+  print_success "Upgrading Packages..."
+  sudo apt-get upgrade -y > /dev/null 2>&1
+fi
+
+# Allow mosh connections
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-mosh-on-a-vps
+sudo ufw allow 60000:61000/udp
+
+print_success "Utilities Installed"
