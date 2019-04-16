073d88c9921886139ee1b58997ad97497b21f45f
PJ Fancher <pj@digitalbrands.com>
2019-04-16 20:44:44 +0000

    Updated macos

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 . "$HOME/dotfiles/install/utils.sh"
 verify_osx
-
 clear
 
-. "$HOME/dotfiles/install/banner.sh" \
-&& "\n\nSetting Up Mac OS X\n\n"
+print_in_purple "\n\nSetting Up Mac OS X\n\n"
 
 # Load Scripts
 cd "$(dirname "${BASH_SOURCE[0]}")" \
@@ -13,5 +11,5 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
   && . "macos/brew.sh" \
   && cd $HOME
 
-#Complete
+# Complete
 print_in_purple "\n\nMac OS X Setup Complete! Enjoy!\n\n"
