849b09fa696ee169cfb5ca7464287a6a74486239
PJ Fancher <pj@digitalbrands.com>
2019-04-17 11:11:27 -0400

    master@unknown: Updates for mac

--- .zsh/lib/theme-and-appearance.zsh
+++ .zsh/lib/theme-and-appearance.zsh
@@ -25,7 +25,7 @@ if [[ "$DISABLE_LS_COLORS" != "true" ]]; then
 
     # only use coreutils ls if there is a dircolors customization present ($LS_COLORS or .dircolors file)
     # otherwise, gls will use the default color scheme which is ugly af
-    [[ -n "$LS_COLORS" || -f "$HOME/.dircolors" ]] && gls --color -d . &>/dev/null && alias ls='gls --color=tty'
+    #[[ -n "$LS_COLORS" || -f "$HOME/.dircolors" ]] && gls --color -d . &>/dev/null && alias ls='gls --color=tty'
   else
     # For GNU ls, we use the default ls color theme. They can later be overwritten by themes.
     if [[ -z "$LS_COLORS" ]]; then
