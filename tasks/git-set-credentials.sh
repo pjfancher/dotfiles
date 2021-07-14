4176d4ef88ff437838a9a56e5ff64d19c5f6ade3
PJ Fancher <pj@digitalbrands.com>
2021-07-14 18:39:43 +0000

    master@pj-2004-02: master@pj-2004-02: Used standard store file # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # Date: Wed Jul 14 18:39:43 2021 +0000 # # On branch master # Your branch is ahead of 'origin/master' by 1 commit. # (use "git push" to publish your local commits) # # Changes to be committed: # modified: git-set-credentials.sh #

--- tasks/git-set-credentials.sh
+++ tasks/git-set-credentials.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 # Generate .git-credentials file based on Environment Variables
 
+# Required:
+# $GIT_USER - git Username (ex: db-pj)
+# $GIT_PASS - git Personal Access Token (https://github.com/settings/tokens)
+#*****************************************************************************
+
 # Get sudo upfront
 #*****************************************************************************
 sudo -v
 
-# Required:
-# $GIT_USER - git Username (ex: db-pj)
-# $GIT_PASS - git Personal Access Token (https://github.com/settings/tokens)
+# Vars
+#*****************************************************************************
+GC=~/.git-credentials
+ERROR=false
 
 # Source ~/dotfiles/.profile for local vars if it exists
 #*****************************************************************************
@@ -25,6 +31,7 @@ fi
 #*****************************************************************************
 if [ -z "$GIT_USER" ]; then
   printf "\$GIT_USER NOT set.\nTry exporting \$GIT_USER as your github username in your ~/.{bash_}profile\n\n"
+  ERROR=true
 else
   printf "\$GIT_USER is set\n"
 fi
@@ -33,6 +40,7 @@ fi
 #*****************************************************************************
 if [ -z "$GIT_PASS" ]; then
   printf "\$GIT_PASS NOT set.\nTry exporting \$GIT_PASS to your github Personal Access Token in your ~/.{bash_}profile\nhttps://github.com/settings/tokens\n\n"
+  ERROR=true
 else
   printf "\$GIT_PASS is set\n"
 fi
@@ -40,13 +48,12 @@ fi
 #*****************************************************************************
 # Try to generate the ~/.git-credentials file
 #*****************************************************************************
-if [ -z "$GIT_USER" ] || [ -z "$GIT_PASS" ]; then
+if [ "$ERROR" = true ]; then
   printf "\nERROR: Could not generate ${GC} file\nCheck your \$GIT_USER and \$GIT_PASS Environment variables.\n\n";
   exit 1
 else
   # Define Credential Store Method for Git
   #*****************************************************************************
-  GC=~/.git-${GIT_USER}-credentials
   printf "Setting .git/config to cache credentials on disk at ${GC}\n\n"
   git config credential.helper "store --file ${GC}"
 
