3975c02b82290be5825e76a51d5f2f5edf564019
PJ Fancher <pj@digitalbrands.com>
2023-07-26 20:25:12 +0000

    master@pj-nas: Updated ,convert-divx-mp4.sh

--- .local/bin/,convert-divx-mp4.sh
+++ .local/bin/,convert-divx-mp4.sh
@@ -1,6 +1,36 @@
-#!/bin/sh
+#!/bin/bash
 
-for var in "$@"
-do
-  ffmpeg -i "$var" -acodec aac -vcodec libx264 "${var.%}.mp4"
-done
+# Check if an argument was provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 input.divx"
+  exit 1
+fi
+
+# Check if FFmpeg is installed
+if ! command -v ffmpeg &> /dev/null; then
+  echo "FFmpeg is not installed. Please install FFmpeg and try again."
+  exit 1
+fi
+
+# Get the input file name and extension
+input_file="$1"
+input_extension="${input_file##*.}"
+
+# Check if the input file is an MKV file
+if [ "$input_extension" != "divx" ]; then
+  echo "Input file must be an DIVX file."
+  exit 1
+fi
+
+# Set the output file name and extension
+output_file="${input_file%.*}.mp4"
+
+# Convert the file using FFmpeg
+ffmpeg -i "$input_file" -vcodec libx264 -acodec aac "$output_file"
+
+# Check if the conversion was successful
+if [ $? -eq 0 ]; then
+  echo "Conversion complete. Output file: $output_file"
+else
+  echo "Conversion failed."
+fi
